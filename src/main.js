55d5e80ebb878dce79c965546f08dce4ae9cbe72
Earl of Tureyev <lider-1@mail.ru>
2026-05-03 00:47:26 +0500

    Comlete analyzeSalesData implementation

--- src/main.js
+++ src/main.js
@@ -45,11 +45,101 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
   // @TODO: Проверка входных данных
+
+  if (
+    !data ||
+    !Array.isArray(data.sellers) ||
+    !Array.isArray(data.products) ||
+    !Array.isArray(data.purchase_records) ||
+    data.sellers.length === 0 ||
+    data.products.length === 0 ||
+    data.purchase_records.length === 0
+  ) {
+    throw new Error("Некорректные входные данные");
+  }
+
   // @TODO: Проверка наличия опций
+
+  const { calculateRevenue, calculateBonus } = options;
+  if (
+    typeof calculateRevenue !== "function" ||
+    typeof calculateBonus !== "function"
+  ) {
+    console.log("calculateRevenue:", calculateRevenue);
+    console.log("calculateBonus:", calculateBonus);
+    throw new Error("Не найдены необходимые функции");
+  }
+
   // @TODO: Подготовка промежуточных данных для сбора статистики
+
+  const sellerStats = data.sellers.map((seller) => ({
+    id: seller.id,
+    name: `${seller.first_name} ${seller.last_name}`,
+    revenue: 0,
+    profit: 0,
+    sales_count: 0,
+    products_sold: {},
+  }));
+
   // @TODO: Индексация продавцов и товаров для быстрого доступа
+
+  const sellerIndex = Object.fromEntries(
+    sellerStats.map((seller) => [seller.id, seller]),
+  );
+  const productIndex = Object.fromEntries(
+    data.products.map((product) => [product.sku, product]),
+  );
+
   // @TODO: Расчет выручки и прибыли для каждого продавца
+
+  data.purchase_records.forEach((record) => {
+    const seller = sellerIndex[record.seller_id];
+
+    seller.sales_count += 1;
+    seller.revenue += record.total_amount;
+
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku];
+
+      const cost = product.purchase_price * item.quantity;
+
+      const revenue = calculateRevenue(item, product);
+
+      const profit = revenue - cost;
+
+      seller.profit += profit;
+
+      if (!seller.products_sold[item.sku]) {
+        seller.products_sold[item.sku] = 0;
+      }
+      seller.products_sold[item.sku] += item.quantity;
+    });
+  });
+
   // @TODO: Сортировка продавцов по прибыли
+
+  sellerStats.sort((a, b) => b.profit - a.profit);
+
   // @TODO: Назначение премий на основе ранжирования
+
+  sellerStats.forEach((seller, index) => {
+    seller.bonus = calculateBonus(index, sellerStats.length, seller);
+
+    seller.top_products = Object.entries(seller.products_sold)
+      .map(([sku, quantity]) => ({ sku, quantity }))
+      .sort((a, b) => b.quantity - a.quantity)
+      .slice(0, 10);
+  });
+
   // @TODO: Подготовка итоговой коллекции с нужными полями
+
+  return sellerStats.map((seller) => ({
+    seller_id: seller.id,
+    name: seller.name,
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
+    sales_count: seller.sales_count,
+    top_products: seller.top_products,
+    bonus: +seller.bonus.toFixed(2),
+  }));
 }
